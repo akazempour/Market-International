e701ec732030992523d25d0d965ac7aeec280570
akazempour <akazempour@hotmail.com>
2015-02-08 18:55:45 +0100

    Test

---
+++ Market-International/AdminDetailAdd.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="AdminDetailAdd.aspx.cs" Inherits="Market_International.AdminDetailAdd" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        label {
+            float: left;
+            width: 20%;
+            text-align: right;
+        }
+
+        .leftassign {
+            margin-left: 20%;
+        }
+    </style>
+
+    <script src="//code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        function ShowpImagePreview(input, FileId) {
+            FileId = '#' + FileId;
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $(FileId).attr('src', e.target.result);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    </script>
+
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <label>Show:</label><asp:CheckBox ID="Show" runat="server" Checked="True" />
+
+    <br />
+    <br />
+    <label>Title:</label><asp:TextBox ID="TitleText" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ForeColor="Red"  ControlToValidate="TitleText" ErrorMessage="***Title is require"></asp:RequiredFieldValidator>
+    <br />
+    <br />
+    <label>SubTitle:</label><asp:TextBox ID="SubTitleText" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ForeColor="Red"  ControlToValidate="SubTitleText" ErrorMessage="***Subtitle is require"></asp:RequiredFieldValidator>
+
+    <br />
+    <br />
+    <label>Desc1</label><asp:TextBox ID="Desc1Text" runat="server" Height="155px" TextMode="MultiLine" Width="399px"></asp:TextBox>
+    <br />
+    <br />
+    <label>Desc2:</label><asp:TextBox ID="Desc2Text" Height="155px" Width="399px" runat="server" TextMode="MultiLine"></asp:TextBox>
+    <br />
+    <br />
+    <asp:RadioButton ID="Scroll" Text="Main Image Scroll" GroupName="PageControl" runat="server" />
+    <br />
+    <asp:RadioButton ID="MainPage" Text="Main page" GroupName="PageControl" runat="server" />
+    <br />
+    <asp:RadioButton ID="DetailPage" Text="Page detail" GroupName="PageControl" runat="server"  Checked="true"/>
+    <br />
+    <br />
+    <br />
+
+
+    <label>Image1:</label><asp:Image ID="ImgPrv1" Height="155px" Width="399px" runat="server" /><br />
+    <asp:FileUpload CssClass="leftassign" ID="flupImage1" runat="server" onchange="ShowpImagePreview(this,'ContentPlaceHolder1_ImgPrv1');" />
+    <br /><br />
+    <label>Image2:</label><asp:Image ID="ImgPrv2" Height="155px" Width="399px" runat="server" /><br />
+    <asp:FileUpload CssClass="leftassign" ID="flupImage2" runat="server" onchange="ShowpImagePreview(this,'ContentPlaceHolder1_ImgPrv2');" />
+    <br /><br />
+    <label>Image3:</label><asp:Image ID="ImgPrv3" Height="155px" Width="399px" runat="server" /><br />
+    <asp:FileUpload CssClass="leftassign" ID="flupImage3" runat="server" onchange="ShowpImagePreview(this,'ContentPlaceHolder1_ImgPrv3');" />
+    <br /><br />
+    <label>Image4:</label><asp:Image ID="ImgPrv4" Height="155px" Width="399px" runat="server" /><br />
+    <asp:FileUpload CssClass="leftassign" ID="flupImage4" runat="server" onchange="ShowpImagePreview(this,'ContentPlaceHolder1_ImgPrv4');" />
+    <br /><br />
+    <label>Image5:</label><asp:Image ID="ImgPrv5" Height="155px" Width="399px" runat="server" /><br />
+    <asp:FileUpload CssClass="leftassign" ID="flupImage5" runat="server" onchange="ShowpImagePreview(this,'ContentPlaceHolder1_ImgPrv5');" />
+    <br /><br /><br />
+    <asp:Button CssClass="leftassign" ID="Submit"  runat="server" Text="Upload" OnClick="Summit" />
+
+
+    <br />
+    <asp:HiddenField ID="FieldAction"  runat="server" />
+    <asp:HiddenField ID="FieldId"  runat="server" />
+
+
+</asp:Content>
---
+++ Market-International/AdminDetailAdd.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Market_International
+{
+    public partial class AdminDetailAdd : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string action = Request["action"];
+                FieldAction.Value = action;
+                FieldId.Value = Request["id_subcat"];
+                if (action == "edit")
+                {
+                    string ssss = "aaa";  
+                }
+
+            }
+        }
+
+        protected void Summit(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                string action = FieldAction.Value ;
+                int SubCatId = Convert.ToInt32(FieldId.Value);
+                string myUniqueFileName1 = null;
+                string myUniqueFileName2 = null;
+                string myUniqueFileName3 = null;
+                string myUniqueFileName4 = null;
+                string myUniqueFileName5 = null;
+
+                if (flupImage1.PostedFile != null && flupImage1.PostedFile.FileName != "")
+                {
+                    string imgName1 = flupImage1.FileName;
+                    string ext = System.IO.Path.GetExtension(imgName1);
+                    myUniqueFileName1 = string.Format(@"{0}" + ext, Guid.NewGuid());
+                    string imgPath = "image/" + myUniqueFileName1;                    
+                    flupImage1.SaveAs(Server.MapPath(imgPath));
+                }
+
+                if (flupImage2.PostedFile != null && flupImage2.PostedFile.FileName != "")
+                {
+                    string imgName1 = flupImage2.FileName;
+                    string ext = System.IO.Path.GetExtension(imgName1);
+                    myUniqueFileName2 = string.Format(@"{0}" + ext, Guid.NewGuid());
+                    string imgPath = "image/" + myUniqueFileName2;
+                    flupImage2.SaveAs(Server.MapPath(imgPath));
+                }
+
+                if (flupImage3.PostedFile != null && flupImage3.PostedFile.FileName != "")
+                {
+                    string imgName1 = flupImage3.FileName;
+                    string ext = System.IO.Path.GetExtension(imgName1);
+                    myUniqueFileName3 = string.Format(@"{0}" + ext, Guid.NewGuid());
+                    string imgPath = "image/" + myUniqueFileName3;
+                    flupImage3.SaveAs(Server.MapPath(imgPath));
+                }
+
+                if (flupImage4.PostedFile != null && flupImage4.PostedFile.FileName != "")
+                {
+                    string imgName1 = flupImage4.FileName;
+                    string ext = System.IO.Path.GetExtension(imgName1);
+                    myUniqueFileName4 = string.Format(@"{0}" + ext, Guid.NewGuid());
+                    string imgPath = "image/" + myUniqueFileName4;
+                    flupImage4.SaveAs(Server.MapPath(imgPath));
+                }
+
+                if (flupImage5.PostedFile != null && flupImage5.PostedFile.FileName != "")
+                {
+                    string imgName1 = flupImage5.FileName;
+                    string ext = System.IO.Path.GetExtension(imgName1);
+                    myUniqueFileName5 = string.Format(@"{0}" + ext, Guid.NewGuid());
+                    string imgPath = "image/" + myUniqueFileName5;
+                    flupImage5.SaveAs(Server.MapPath(imgPath));
+                }
+
+                if (action == "add")
+                {
+                    string Title = TitleText.Text;
+                    string SubTitle = SubTitleText.Text;
+                    int show = Show.Checked ? 1 : 0;
+                    string Desc1 = Desc1Text.Text;
+                    string Desc2 = Desc2Text.Text;
+                    int ScreenImage = 0;
+                    if (Scroll.Checked)
+                        ScreenImage = 1;
+                    else if (MainPage.Checked)
+                        ScreenImage = 2;
+                    else if (DetailPage.Checked)
+                        ScreenImage = 3;
+
+                    sql_object sql_obj = new sql_object();
+                    sql_obj.DetailAdd(SubCatId, Title, SubTitle, Desc1, Desc2, myUniqueFileName1, myUniqueFileName2, myUniqueFileName3,
+                        myUniqueFileName4, myUniqueFileName5, ScreenImage, show);
+
+
+                }
+
+                string url = "./Admin_Home.aspx";
+                Response.Redirect(url); 
+
+            }
+        }
+
+
+    }
+}
